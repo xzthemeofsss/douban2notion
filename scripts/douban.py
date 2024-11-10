2bdd64dc8be7172c0cefcddac3bd58d47fcab13f
xzthemeofsss <xzthemeofsss@gmail.com>
2024-11-10 13:53:03 +0800

    Update douban.py

--- scripts/douban.py
+++ scripts/douban.py
@@ -93,8 +93,11 @@ def insert_movie():
         results.extend(fetch_subjects(douban_name, "movie", i))
     for result in results:
         movie = {}
-        print(result)
         subject = result.get("subject")
+        if(subject == None){
+            print(result)
+            continue
+        }
         if(subject.get("title")=="未知电影" or subject.get("title")=="未知电视剧") and subject.get("url") in unknown_dict:
             unknown = unknown_dict.get(subject.get("url"))
             subject["title"] = unknown.get("title")
